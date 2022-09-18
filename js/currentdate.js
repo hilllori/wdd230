f15b0a19c0863f415e90ebab87f64b7d03eaa33f
hilllori <hilllo@byui.edu>
2022-09-17 19:47:26 -0500

    tes 3

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,2 +1,5 @@
 const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+let text = document.lastModified;
+document.getElementById("demo").innerHTML = text;
