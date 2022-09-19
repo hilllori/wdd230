28ffc16ab59003d871152ecd5e159092232a88c6
hilllori <hilllo@byui.edu>
2022-09-19 12:13:03 -0500

    date

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,5 +1,2 @@
-const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
-
 let text = document.lastModified;
 document.getElementById("demo").innerHTML = text;
